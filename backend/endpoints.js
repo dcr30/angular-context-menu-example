3ae6590c200f55126939ca7fcba5a838964e6c86
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-08-04 22:24:20 +0300

    Коллекция пользователей

---
+++ backend/endpoints.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const mongodb = require('mongodb');
+
+const USERS_COLLECTION = 'users';
+const RANDOM_USERS_COUNT = 10;
+
+function handleError(res, message, description, code=500) {
+    console.log(`Error: ${message} ${description ? '(' + description + ')' : ''}`);
+    res.status(code).json({
+        error: message,
+        description
+    });
+}
+
+function createRandomUsers(database, count) {
+    let groups = ['everyone', 'moderator', 'admin'];
+    let usersCollection = database.collection(USERS_COLLECTION);
+    for (let i = 0; i < count; i++) {
+        let name = 'RandomUser' + (i + 1);
+        usersCollection.insertOne({
+            name,
+            email: name + '@example.com',
+            group: groups[Math.floor(Math.random() * groups.length)],
+            online: Math.random() > 0.3,
+            avatar100: 'https://api.adorable.io/avatars/100/' + name,
+        });
+    }
+}
+
+module.exports = (app, database) => {
+    // Имя пользователя должно быть уникальным
+    database.collection(USERS_COLLECTION).createIndex({name: 1}, {unique: true});
+
+    // Инициализация коллекции пользователей
+    database.collection(USERS_COLLECTION).count((error, count) => {
+        if (!error && count == 0) {
+            createRandomUsers(database, RANDOM_USERS_COUNT);
+        }
+    });
+    
+    // Получение списка всех пользователей
+    app.get('/users', (req, res) => {
+        database.collection(USERS_COLLECTION).find({}).toArray((error, documents) => {
+            if (error) {
+                handleError(res, 'Failed do get users', error.message);
+            } else {
+                res.status(200).json(documents);
+            }
+        });
+    });
+
+    // Добавление нового пользователя
+    app.post('/users', (req, res) => {
+        let newUser = req.body;
+        delete newUser._id;
+        newUser.group = 'everyone';
+
+        if (!newUser.name) {
+            handleError(res, 'Invalid user data', 'Username is incorrect', 400);
+        }
+
+        database.collection(USERS_COLLECTION).insertOne(newUser, (error, result) => {
+            if (error) {
+                handleError(res, 'Failed to create user', error.message);
+            } else {
+                res.status(201).json({id: result.insertedId});
+            }
+        });
+    });
+
+    // Обновление профиля пользователя
+    app.put('/users/:id', (req, res) => {
+        let updatedUser = req.body;
+        let userId = new mongodb.ObjectID(updatedUser._id);
+        delete updatedUser._id;
+
+        database.collection(USERS_COLLECTION).updateOne({_id: userId}, (error) => {
+            if (error) {
+                handleError(res, 'Failed to update user', error.message);
+            } else {
+                res.status(204).end();
+            }
+        });
+    });
+};
--- backend/main.js
+++ backend/main.js
@@ -5,17 +5,28 @@ const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongodb.MongoClient.connect("mongodb://localhost:27017/myapi", (error, database) => {
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    next();
+});
+
+mongodb.MongoClient.connect("mongodb://localhost:27017/angular_context_menu", (error, database) => {
     if (error) {
         console.log("Error connecting to database");
         console.error(error);
         process.exit(1);
-    }
-    console.log("Database connection success");
-    const routes = require('./routes.js')(app, database);
+    }    
+    console.log("Database connection success");        
+    require('./endpoints.js')(app, database);
+
+    console.log("Starting server...")
     var server = app.listen(3000, () => {
         console.log('Listening on port: ' + server.address().port);
     });
